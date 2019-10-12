6b9cdc2d6163a6138e3424cd0374eaa90b5149ed
Michael Tesfaye <mtesfaye102@gmail.com>
2019-10-12 16:56:52 -0400

    Importing Project

---
+++ BookWorld/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookWorld.Models;
+
+namespace BookWorld.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Index()
+        {
+            var book = new Book() {Name = "Illiad"};
+
+            return View();
+        }
+    }
+}
