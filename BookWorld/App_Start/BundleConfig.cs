e143d3423639feb78f633380c0e40eda1c4310dc
Michael Tesfaye <mtesfaye102@gmail.com>
2019-10-19 14:00:05 -0400

    Rental feature - Toastr
    Added design to UI by installing Toastr and using it to return a notification to the user if the rental action succeeded or not.

--- BookWorld/App_Start/BundleConfig.cs
+++ BookWorld/App_Start/BundleConfig.cs
@@ -14,7 +14,8 @@ namespace BookWorld
                         "~/Scripts/bootbox.js",
                         "~/Scripts/DataTables/jquery.dataTables.js",
                         "~/Scripts/DataTables/dataTables.bootstrap.js",
-                        "~/Scripts/typeahead.bundle.js"
+                        "~/Scripts/typeahead.bundle.js",
+                        "~/Scripts/toastr.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -29,6 +30,7 @@ namespace BookWorld
                       "~/Content/bootstrap-journal.css",
                       "~/Content/DataTables/css/dataTables.bootstrap.css",
                       "~/Content/typeahead.css",
+                      "~/Content/toastr.css",
                       "~/Content/site.css"));
         }
     }
